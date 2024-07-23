0f953b8087a963f11cc659722b77bd140257a5d4
Jose R. Rodriguez <jrodriguez18@gmail.com>
2024-07-22 21:07:02 -0400

    Added two way binding to input elements

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -9,6 +9,11 @@ import { FormsModule } from "@angular/forms";
   styleUrl: "./user-input.component.css",
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+  
   onSubmit() {
     console.log("submit works");
   }
